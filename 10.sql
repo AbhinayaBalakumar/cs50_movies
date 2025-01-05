e5897f50ac227662b6f9529590d280c6bbfa7711
AbhinayaBalakumar <161173993+AbhinayaBalakumar@users.noreply.github.com>
2025-01-05 14:14:39 -0500

    Add files via upload

---
+++ 10.sql
@@ -0,0 +1,13 @@
+-- In 10.sql, write a SQL query to list the names of all people who have directed a movie that received a rating of at least 9.0.
+-- Your query should output a table with a single column for the name of each person.
+-- If a person directed more than one movie that received a rating of at least 9.0, they should only appear in your results once.
+SELECT DISTINCT people.name
+FROM people
+JOIN movies ON movies.id = directors.movie_id
+JOIN directors ON people.id = directors.person_id
+JOIN ratings ON ratings.movie_id = directors.movie_id
+WHERE ratings.rating >= 9.0;
+
+-- cat 10.sql | sqlite3 movies.db > output.txt
+-- check50 cs50/problems/2025/x/movies
+
